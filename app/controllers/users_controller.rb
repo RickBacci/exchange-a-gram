6ad493d5de5ba9bd47f1792bec66bef82e4bc694
Rick Bacci <rickbacci@gmail.com>
2015-07-21 14:46:10 -0600

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
+     @client ||= Instagram.client(:access_token => current_user.token)
   end
 
   def show
-     @client = Instagram.client(:access_token => current_user.token)
+     @client ||= Instagram.client(:access_token => current_user.token)
   end
 end
 
