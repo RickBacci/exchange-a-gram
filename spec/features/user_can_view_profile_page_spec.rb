486807d9f553f5d68dbda166d81c7efc1b21b398
Rick Bacci <rickbacci@gmail.com>
2015-07-23 12:20:30 -0600

    feature test broken
    - capybara can't find the link

--- spec/features/user_can_view_profile_page_spec.rb
+++ spec/features/user_can_view_profile_page_spec.rb
@@ -17,10 +17,11 @@ describe User, type: :feature do
       click_link "login"
 
       expect(current_path).to eq(root_path)
-#      expect(page).to have_content('Ricky')
 
-      click_link "brownieelf175"
-
+        save_and_open_page
+      within(".profile") do
+        click_link "brownieelf175"
+      end
       expect(page.status_code).to eq 200
       expect(page).to have_content('Profile')
     end
