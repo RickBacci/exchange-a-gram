116be51be40de186dad36e61836801ce4cd3a7dc
Richard Bacci <richardbacci@Richards-MacBook-Pro.local>
2015-07-25 14:20:17 -0600

    fix html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
   def index
-    @user = current_user.instagram_client.user(params[:instagram_id]) if current_user
-    if params['name']
-      @search_user = current_user.instagram_client.user_search(params['name']).first.id
-      if @search_user
-        redirect_to user_path(@search_user)
-      else
-        render :root
-      end
-    end
+    # @user = current_user.instagram_client.user(params[:instagram_id]) if current_user
+    # if params['name']
+    #   @search_user = current_user.instagram_client.user_search(params['name']).first.id
+    #   if @search_user
+    #     redirect_to user_path(@search_user)
+    #   else
+    #     render :root
+    #   end
+    # end
   end
 
   def show
-    @user = current_user.instagram_client.user(params[:instagram_id])
+#    @user = current_user.instagram_client.user(params[:instagram_id])
   end
 end
 
--- spec/features/user_can_view_profile_page_spec.rb
+++ spec/features/user_can_view_profile_page_spec.rb
@@ -12,15 +12,15 @@ describe User, type: :feature do
   it 'can view profile page' do
     VCR.use_cassette("profile") do
       visit root_path
-
       assert_equal 200, page.status_code
       click_link "login"
 
       expect(current_path).to eq(root_path)
-
+      save_and_open_page
       # within("li a.profile") do
       #   click_link "brownieelf175"
       # end
+
       # expect(page.status_code).to eq 200
       # expect(page).to have_content('Profile')
     end
